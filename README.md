19cc5c2bbc688e656e3c6db2513b5160f3bfaa5c
Marcos V Ramos <viniciusramoscaldas@gmail.com>
2023-05-17 11:47:24 -0300

    Uploading settings and base projects

---
+++ README.md
@@ -0,0 +1,11 @@
+# Code For Climate Change - FrontEnd
+## Tecnologias Utilizadas:
+
+- React.js
+- Typescript
+- Material UI
+- Axios
+- Redux
+
+## Prototipação:
+
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import Footer from './components/static/teste/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Footer />
+    </>
+  )
+}
+
+export default App;
